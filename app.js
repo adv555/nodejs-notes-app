d98a145811cee1a072b4c76a3981a175cb261a30
Olena Boldysheva <adv55577@gmail.com>
2022-04-10 01:02:37 +0300

    added google auth

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const path = require('path')
 const notesRouter = require('./routes/notes')
+const authRouter = require('./routes/auth/auth.router')
 
 const app = express()
 
@@ -11,10 +13,10 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
+app.use('/auth', authRouter)
 app.use('/notes', notesRouter)
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+app.use('/link', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 app.use((err, req, res, next) => {
