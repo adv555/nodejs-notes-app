7f8ae887dea687ed61895124e3f9e4879435f7c1
Olena Boldysheva <adv55577@gmail.com>
2022-04-11 15:20:26 +0300

    added swagger

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 const notesRouter = require('./routes/notes')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
 
 const app = express()
 
@@ -12,6 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/notes', notesRouter)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
